62c0144579b04d9d073d60409c45406414bd1841
BuildTools <unconfigured@null.spigotmc.org>
2021-06-21 15:46:56 +0200

    Finished the command system I believe

--- src/main/java/plzmercy/paralian/parautils/command/BukkitCommand.java
+++ src/main/java/plzmercy/paralian/parautils/command/BukkitCommand.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.Validate;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.Plugin;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public class BukkitCommand extends org.bukkit.command.Command{
     private Plugin owningPlugin;
     private CommandExecutor ex;
-
+    protected BukkitCompleter completer;
 
     protected BukkitCommand(String label, CommandExecutor executor, Plugin owner){
         super(label);
@@ -42,4 +43,36 @@ public class BukkitCommand extends org.bukkit.command.Command{
         return success;
     }
     //TODO: Add the a tab completion
+
+    @Override
+    public List<String> tabComplete(CommandSender sender, String alias, String[] args)
+            throws CommandException, IllegalArgumentException {
+        Validate.notNull(sender, "Sender cannot be null");
+        Validate.notNull(args, "Arguments cannot be null");
+        Validate.notNull(alias, "Alias cannot be null");
+
+        List<String> completions = null;
+        try {
+            if (completer != null) {
+                completions = completer.onTabComplete(sender, this, alias, args);
+            }
+            if (completions == null && ex instanceof TabCompleter) {
+                completions = ((TabCompleter) ex).onTabComplete(sender, this, alias, args);
+            }
+        } catch (Throwable ex) {
+            StringBuilder message = new StringBuilder();
+            message.append("Unhandled exception during tab completion for command '/").append(alias).append(' ');
+            for (String arg : args) {
+                message.append(arg).append(' ');
+            }
+            message.deleteCharAt(message.length() - 1).append("' in plugin ")
+                    .append(owningPlugin.getDescription().getFullName());
+            throw new CommandException(message.toString(), ex);
+        }
+
+        if (completions == null) {
+            return super.tabComplete(sender, alias, args);
+        }
+        return completions;
+    }
 }
